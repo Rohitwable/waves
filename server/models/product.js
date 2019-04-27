eb242429af49e7dbb1a7c42096050132fcdbaec0
Rohitwable <rohitwable46@gmail.com>
2019-04-27 22:24:22 +0530

    product schema , route

---
+++ server/models/product.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const Brand = require('./brand')
+
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: 1,
+        maxlength: 100
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 100000
+    },
+    price: {
+        type: Number,
+        required: true,
+        maxlength: 255   
+    },
+    brand: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Brand',
+        required: true
+    },
+    shipping: {
+        type: Boolean,
+        required: true
+    },
+    available: {
+        type: Boolean,
+        required: true
+    },
+    wood: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Wood',
+        required: true
+    },
+    frets: {
+        type: Number,
+        required: true
+    },
+    sold: {
+        type: Number,
+        maxlength: 100,
+        default: 0
+    },
+    publish: {
+        required: true,
+        type: Boolean
+    },
+    images: {
+        type: Array,
+        default: []
+    }
+
+},{timestamps: true})
+
+const Product = mongoose.model('Product',productSchema)
+module.exports = { Product }
